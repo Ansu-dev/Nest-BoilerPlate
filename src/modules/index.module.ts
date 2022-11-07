eabaa63260533de67fa838eb3148d339e10c4847
Ansu-dev <ansu1007@noutecompany.com>
2022-11-07 17:53:03 +0900

    Feat(first-commit):entity settings

---
+++ src/modules/index.module.ts
@@ -0,0 +1,12 @@
+import { Module } from "@nestjs/common";
+import { AuthModule } from "./auth/auth.module";
+import { UserModule } from "./user/user.module";
+
+@Module({
+    imports: [
+        UserModule,
+        AuthModule
+    ]
+})
+
+export class IndexModule { }

f3509424477f5bf1c3d73572d06b6da0aec72180
Ansu-dev <ansu1007@noutecompany.com>
2022-11-21 11:03:46 +0900

    Feat(global-pipe):global pipe set up

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Logger, ValidationPipe } from '@nestjs/common'
+import { BadRequestException, Logger, ValidationPipe } from '@nestjs/common'
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { LoggingInterceptor } from './interceptors/logging.interceptor';
+import { ValidationError } from 'class-validator';
 
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  //cors 설정
+  //cors SetUp
   let envAllow = process.env.ALLOW_CORS_LIST
   let allowList = envAllow.split(',')
   app.enableCors({
@@ -27,14 +28,29 @@ async function bootstrap() {
     ignoreGlobalPrefix: true
   })
   SwaggerModule.setup('/swagger', app, document)
-  //서버 루트 url
+  //Server Root URL
   app.setGlobalPrefix('v1/api')
   app.useGlobalPipes(new ValidationPipe())
-  
-  //글로벌 로깅 인터셉터
+
+  //Global Loggin Interceptor
   app.useGlobalInterceptors(new LoggingInterceptor())
 
-  //서버 포트
+  //Global ValidationPipe
+  app.useGlobalPipes(
+    new ValidationPipe({
+      exceptionFactory: (err: ValidationError[]) => {
+        new BadRequestException({
+          status: 400,
+          data: {
+            resultCode: -1,
+            data: null
+          }
+        })
+      }
+    })
+  )
+
+  //Server Listen
   const port: number = Number(process.env.SERVER_PORT)
   await app.listen(port, () => {
     Logger.log(`SERVER - ${port}PORT CONNECTED`)
